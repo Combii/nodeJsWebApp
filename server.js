ff6373f98988b52ca41f4512ef3a79a18141b7f9
Combii <davidstovle@gmail.com>
2017-09-08 10:22:36 +0200

    New stuff

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http');
 var server = http.createServer(function(req, res){
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200, {'Content-type' : 'text/json'});
-    res.write('{"title" : "Hello JSON web API", "text" : "Text fra min API"}');
+    res.write('{"title" : "Hello JSON web API!", "text" : "Text fra min API"}');
     res.end();
 });
 
